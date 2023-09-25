93d93669faf17d01022bfd6d63a68610c265d8fb
Yargrinders <yargrindersp20@gmail.com>
2023-09-25 14:06:14 +0200

    Hotel v1

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,6 +1,15 @@
 // example.js
 export function burgerM() {
-    console.log("Я Burger!");
+    document.addEventListener("DOMContentLoaded", function () {
+        const burgerMenu = document.querySelector(".burger-menu");
+        const navList = document.querySelector(".header_top__nav__list");
+    
+        burgerMenu.addEventListener("click", function () {
+            navList.classList.toggle("active");
+            burgerMenu.classList.toggle("active");
+        });
+    });
+    
 }
 
 export default burgerM;
