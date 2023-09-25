bbd519880c0f40b7bedf614ff0756dab3f77703c
Yargrinders <yargrindersp20@gmail.com>
2023-09-25 22:12:56 +0200

    fix js

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@ import burgerM from './modules/burger.js';
 import scrollToBottom from './modules/scroll-down.js';
 import counT from './modules/count.js'
 import accorDion from './modules/accordion.js'
+import actiVe from './modules/active.js'
 
 exampLe();
 burgerM();
 scrollToBottom();
 counT();
 accorDion();
+actiVe();
